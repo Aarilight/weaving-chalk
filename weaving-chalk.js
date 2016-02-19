1e4c4fe065ab4de7e2e267cf17ecfcef7f05edc6
Mackenzie McClane <aaritak@gmail.com>
2016-02-19 01:22:44 -0600

    Add support to strip styles back out

--- weaving-chalk.js
+++ weaving-chalk.js
@@ -3,7 +3,7 @@ var chalk = require("chalk");
 module.exports = function (library) {
     return {
         name: "weaving-chalk",
-        match: ["#", library.REGEX(Object.keys(chalk.styles).join("|")), ":", library.CONTENT],
-        return: (_o, style, _c, content) => chalk[style[0]](content)
+        match: ["#", library.REGEX(Object.keys(chalk.styles).join("|") + "|strip"), ":", library.CONTENT],
+        return: (_o, style, _c, content) => chalk[style[0] == "strip" ? "stripColor" : style[0]](content)
     };
 };
